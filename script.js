8d368be101ff5b2a34d54c9d0598d5d3a9f8c1e9
unknown <george.calderon2000@gmail.com>
2025-03-26 02:43:50 -0400

    Issue displaying 4th condition check
    When checking if greater than or equal to, it does not
    check because else if of less than or equal to fulfills
    the check, therefore prompting the final condition to be
    skipped.
    
    Fixes-Bug: n/a
    Implements-Requirement: n/a

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const num1 = 10;
-const num2 = 5;
-const op = "add";
+const num2 = 10;
+// const op = "add";
 
 // the words after the declaration words are variables
 // the declaration tells us what will happen to the
 // variable (const vs let vs var)
 
-switch(op) {  
-    case 'add':
-        console.log(num1 + num2);  // Output: 15
-        break;
-    case 'sub':
-        console.log(num1 - num2);  // Output: 5
-        break;
-    case 'mult':
-        console.log(num1 * num2);  // Output: 50
-        break;
-    case 'div':
-        console.log(num1 / num2); 
-        break;
-    default:
-        console.log("Invalid operator");  
-        // incase we added square to pickle
-}
+// switch(op) {  
+//     case 'add':
+//         console.log(num1 + num2);  // Output: 15
+//         break;
+//     case 'sub':
+//         console.log(num1 - num2);  // Output: 5
+//         break;
+//     case 'mult':
+//         console.log(num1 * num2);  // Output: 50
+//         break;
+//     case 'div':
+//         console.log(num1 / num2); 
+//         break;
+//     default:
+//         console.log("Invalid operator");  
+//         // incase we added square to pickle
+// }
 
 // if (op == "add") {
 //     let sum = num1 + num2;
@@ -39,3 +39,24 @@ switch(op) {
 
 // if is comparing to see if the value of the variable is
 // equal to the value of the string
+// -- PREVIOUS EXERCISE WORK --
+
+if (num1 < num2) {
+    console.log("less than");
+} else if (num1 > num2) {
+    console.log("greater than");
+} else if (num1 <= num2) {
+    console.log("less than OR equal to");
+} else if (num1 >= num2) {
+    console.log("greater than OR equal to");
+}
+
+
+// -- THIS WAS MADE WITH JOSE --
+// if (num1 == num2) {
+//     console.log("same");
+// }   else if (num1 != num2) {
+//         console.log("not the same");
+//     }   else {
+//             console.log ("Something else");
+//         }
